7571d7f32b1f03dd4d30376b5baf3e485f868fd9
yeonseondoh <112925963+yeonseondoh@users.noreply.github.com>
2022-09-26 19:40:05 +0900

    Create hw3.c

---
+++ hw3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(){
+    int num=5;
+
+    for(int i=0;i<num;i++)
+    {
+        for(int j=num-1;j>i;j--)
+        {
+            printf(" ");
+        }
+            
+        for(int j=0;j<2*i+1;j++)
+        {
+        printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+}
